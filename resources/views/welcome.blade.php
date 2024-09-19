a4d28af6efc07af7df60428ca03a645a501d8251
Leontev-Aleksej <leontevaleksej485@gmail.com>
2024-09-19 13:01:17 +0500

    crate project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <h1>Главная страница</h1>
+    <a href="{{route('first')}}">На первую страницу</a>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, ea? Ullam nisi expedita similique, harum praesentium aperiam modi. Inventore provident laudantium obcaecati ullam architecto maxime et illo praesentium, recusandae labore!</p>
+</body>
+</html>
